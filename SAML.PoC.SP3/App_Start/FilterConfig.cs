2cdc1b9817289ea32129c0f77e25fd3d03656a15
Rui Batista <r.batista@rethink.de>
2022-07-18 10:53:53 +0100

    add SP3 project

---
+++ SAML.PoC.SP3/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace SAML.PoC.SP3
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ SAML.PoC.SP3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SAML.PoC.SP3.MvcApplication" Language="C#" %>
